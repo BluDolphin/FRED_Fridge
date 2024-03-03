6dc31866e828f2b309ddb5052650df5f55fdd8e3
BluDolphin <github.com.tablet296@passmail.net>
2024-03-03 23:32:10 +0000

    Update barcodeTest3(works).py

--- Test Code/barcodeTest3(works).py
+++ Test Code/barcodeTest3(works).py
@@ -1,9 +1,10 @@
 import cv2
 from pyzbar import pyzbar
-from picamera2 import Picamera2
+from picamera2 import Picamera2 # Picamera 1 seems to be dead ¯\_(ツ)_/¯
 import time
 
-def scan_barcode_from_image(image_path):
+# Function for scanning image
+def readBarcode(image_path):
     # Read the image from the provided file path
     image = cv2.imread(image_path)
     # Decode barcodes from the image using pyzbar
@@ -12,19 +13,21 @@ def scan_barcode_from_image(image_path):
     for barcode in barcodes:
         # uses UTF-8 encoding
         barcode_data = barcode.data.decode("utf-8")
-        #barcode_type = barcode.type
         print("Barcode Data:", barcode_data)
-        print("Barcode Type:", barcode_type)
+        
+        #barcode_type = barcode.type
+        #print("Barcode Type:", barcode_type)
 
-#Setup camera and configure to 4k resolution
+# Setup camera and configure to 4k resolution
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": (3840, 2160)})
 picam2.configure(camera_config)
 
+# Take an image every second and read data
 while True:
     picam2.start()
     picam2.capture_file("Barcode.jpg")
-    scan_barcode_from_image("Barcode.jpg")
+    readBarcode("Barcode.jpg")
     time.sleep(1)
 
 
